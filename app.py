e4f9b83e5d291ec522733b4d6a3ba71bca1d6986
Scott Kinder <scott.kinder@clickbank.com>
2015-09-03 19:01:15 -0600

    Adding login/logout, making / endpoint require a session auth_token to access.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField
 from wtforms.validators import Required
 from socket import gethostname
+from saw import user_session
 
 hostname = gethostname()
 app = Flask(__name__)
@@ -15,17 +16,34 @@ class LoginForm(Form):
     password = PasswordField('password', validators=[Required()])
 
 
+@app.route('/')
+def index():
+    if session.get('auth_token'):
+        username = session.get('username')
+        auth_token = session.get('auth_token')
+    else:
+        return redirect(url_for('login'))
+    return render_template('base.html', hostname=hostname, username=username)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if session.get('auth_token'):
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
+        session['username'] = form.username.data
+        user = user_session(user_name=session.get('username'))
+        user.set_auth_token(user_pass=form.password.data)
+        session['auth_token'] = user.auth_token
         return redirect(url_for('index'))
     return render_template('login.html', form=form, hostname=hostname)
 
 
-@app.route('/')
-def index():
-    return render_template('base.html', hostname=hostname, username='slk')
+@app.route('/logout')
+def logout():
+    session.pop('auth_token', None)
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
